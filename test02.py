72939b06f09408475d3c78e18fe764f3ca72e61b
madigeyev <mikadi@yandex.ru>
2019-03-23 01:22:09 +0300

    menu,json

--- test02.py
+++ test02.py
@@ -12,6 +12,7 @@ token = "762529566:AAHAV4CmL_ffjPCmisPWLNV16UTZpQfu9BU"
 
 import requests  
 import datetime
+import json
  
 class BotHandler:
  
@@ -46,7 +47,11 @@ class BotHandler:
 
     def show_menu(self, chat_id):
         #params = {'chat_id': chat_id, 'reply_markup':{"ReplyKeyboardMarkup":{"keyboard":[[{"KeyboardButton":{"text":"test"}}]]}}}
-        params = {'chat_id': chat_id, 'reply_markup':{"keyboard":[[{"KeyboardButton":{"text":"test"}}]]}}
+
+
+        reply_markup = {'keyboard': [[{'text': 'first button'}], [{'text': 'second button'}]], 'resize_keyboard': True, 'one_time_keyboard': True}
+        reply_markup = json.dumps(reply_markup)
+        params = {'chat_id': chat_id, 'reply_markup':reply_markup}
         method = 'sendMessage'
         resp = requests.post(self.api_url + method, params)
         return resp
